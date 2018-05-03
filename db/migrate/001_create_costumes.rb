bbdefefe01a606db07ba9ed5e1a91f0699e8278f
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-03 19:37:21 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,9 +10,9 @@
 class CreateCostumes < ActiveRecord::Migration[4.2]
 
   def change
-    create_table :costumes do |table|
-      table.string :name
-      table.float :price
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
       t.integer :size
       t.string :image_url
     end
